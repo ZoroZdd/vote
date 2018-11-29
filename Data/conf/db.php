86953dfd868a1b1878a77d5eca99b95dc9ec33b2
a aron <aaron@MacBook-Pro-5.local>
2018-11-30 00:04:37 +0800

    fix

---
+++ Data/conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysqli',
+  'DB_HOST' => 'dnmp_mysql_1',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'toupiao',   //数据库名
+  'DB_USER' => 'root',   //用户名
+  'DB_PWD' => '123456',   //密码
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
