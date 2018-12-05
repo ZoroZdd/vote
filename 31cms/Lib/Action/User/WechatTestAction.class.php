5d5ef77857be7b3afddea2c5ac353b36c413a5ad
zorozdd <775804500@qq.com>
2018-12-05 19:04:26 +0800

    fix

--- 31cms/Lib/Action/User/WechatTestAction.class.php
+++ 31cms/Lib/Action/User/WechatTestAction.class.php
@@ -25,6 +25,7 @@ class WechatTestAction extends UserAction{
         curl_close($curl);
         //显示获得的数据
         $res = json_decode($data,1);
+        var_dump((string)$data);
         var_dump($data);
         var_dump($res);die;
         return $res['access_token'];
